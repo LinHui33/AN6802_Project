1f5b5df81ed6caff4ec8d7b02d03772b17448101
LIN HUI <97448457+LinHui33@users.noreply.github.com>
2024-04-20 11:04:51 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import numpy as np
 import pandas as pd
 from xgboost import XGBClassifier
 import pickle
 from flask import Flask,request,render_template,jsonify
-
-app = Flask(__name__)
-from static.get_dummies import GetDummies
+from sklearn.base import BaseEstimator, TransformerMixin
 from werkzeug.utils import secure_filename
 
+class GetDummies(BaseEstimator, TransformerMixin):
+    def __init__(self, data_sep=',', col_name_sep='_'):
+        self.dummy_prefix = []
+        
+    # Return self nothing else to do here
+    def fit(self, X, y  = None): 
+        return self
+    
+    # Transformer method for to return transformed data
+    def transform(self, X, y = None):
+        return X
+        
+    # to get feature names    
+    def get_feature_names_out(self, input_features=None):
+        return self.columns
+
+app = Flask(__name__)
 # Load the encoder from the file
 loaded_enc   = pickle.load(open("static/credit_score_multi_class_ord_encoder.pkl", "rb")) 
 loaded_le    = pickle.load(open("static/credit_score_multi_class_le.pkl", "rb"))
